d972a3ecbf947f628e0e98a12ecbebe52e907ac3
Keith Fung <keith.robert.fung@gmail.com>
2016-04-17 13:02:29 -0400

    Add Default Rails Pages into routes

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,10 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  #Default Route for Testing
+  get '/rails/info/properties' => "rails/info#properties"
+  get '/rails/info/routes'     => "rails/info#routes"
+  get '/rails/info'            => "rails/info#index"
+  get '/'                      => "rails/welcome#index"
 end

b9b5a879c35cacebbcd3bc616ea952b50cb5c43d
jhovan1 <jahovanec@gmail.com>
2016-04-20 21:55:17 -0400

    Modify login btn, start user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+  	@user = Users.new(name: "Joe Tester", password: "password")
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
+
+  test "name should be present" do
+  	@user.name = "    "
+  	assert_not @user.valid?
+  end
+  
 end

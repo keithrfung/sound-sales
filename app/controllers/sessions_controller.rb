cc90e3b752472b03428d2002d094eee5167fa658
jhovan1 <jahovanec@gmail.com>
2016-04-23 16:35:27 -0400

    Log a user in; add Session page and helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(userName: params[:session][:userName])
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid username or password'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+
+	# Logs in a user as long as browser is open
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Return the user logged in
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Boolean method to test if a user is logged in; in = true
+  def logged_in?
+    !current_user.nil?
+  end
+
 end

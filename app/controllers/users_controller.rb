1b73bca4c79518e50cc205f1b5155453028bfad6
jhovan1 <jahovanec@gmail.com>
2016-04-23 11:07:38 -0400

    Add userName field to User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,5 @@ class UsersController < ApplicationController
 
   def new
   end
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423140545) do
+ActiveRecord::Schema.define(version: 20160423150632) do
 
   create_table "clients", force: true do |t|
     t.integer  "clientId"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20160423140545) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "userName"
   end
 
 end

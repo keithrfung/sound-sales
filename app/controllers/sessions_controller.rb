7b198e944d55d6552f621084dd99b29dcc8151e0
jhovan1 <jahovanec@gmail.com>
2016-04-23 15:40:38 -0400

    Add create to Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	render 'new'
+    user = User.find_by(userName: params[:session][:userName])
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      flash.now[:danger] = 'Invalid username or password'
+      render 'new'
+    end
   end
 
   def destroy
   end
-  
+
 end

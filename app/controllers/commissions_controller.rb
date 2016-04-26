a350ae520308bb323e588aebc1f1200617a54270
Keith Fung <keith.robert.fung@gmail.com>
2016-04-25 23:51:38 -0400

    Update all forms to have horizontal forms and be formatted correctly with bootstrap

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -28,7 +28,7 @@ class CommissionsController < ApplicationController
 
     respond_to do |format|
       if @commission.save
-        format.html { redirect_to @commission, notice: 'Commission was successfully created.' }
+        format.html { redirect_to @commission, success: 'Commission was successfully created.' }
         format.json { render :show, status: :created, location: @commission }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class CommissionsController < ApplicationController
   def update
     respond_to do |format|
       if @commission.update(commission_params)
-        format.html { redirect_to @commission, notice: 'Commission was successfully updated.' }
+        format.html { redirect_to @commission, success: 'Commission was successfully updated.' }
         format.json { render :show, status: :ok, location: @commission }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class CommissionsController < ApplicationController
   def destroy
     @commission.destroy
     respond_to do |format|
-      format.html { redirect_to commissions_url, notice: 'Commission was successfully destroyed.' }
+      format.html { redirect_to commissions_url, success: 'Commission was successfully destroyed.' }
       format.json { head :no_content }
     end
   end

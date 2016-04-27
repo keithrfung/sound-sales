9760dc49a00f450e907853634ca811c248bd95c9
jhovan1 <jahovanec@gmail.com>
2016-04-27 15:06:26 -0400

    Update Region model; add region_id to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427022835) do
+ActiveRecord::Schema.define(version: 20160427164039) do
 
   create_table "clients", force: true do |t|
     t.string   "first_name"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20160427022835) do
     t.string   "role"
     t.string   "manager"
     t.integer  "manager_id"
+    t.integer  "region_id"
   end
 
   add_index "users", ["manager_id"], name: "index_users_on_manager_id"

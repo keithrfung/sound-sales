188c1e88edd3b04583cbf95639ae9027ef96ea36
jhovan1 <jahovanec@gmail.com>
2016-04-29 20:56:58 -0400

    Added login/logout for session controller and _topbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,12 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-    user = User.find_by(userName: params[:session][:userName])
+   def create
+    user = User.find_by(username: params[:session][:username])
 
-    if user && user.authenticate(params[:session][:password])
+    if params[:session][:password] == User.find_by(username: user.username).password
       log_in user
       redirect_to user
-      
     else
       flash.now[:danger] = 'Invalid username or password'
       render 'new'
@@ -17,6 +16,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 
 end

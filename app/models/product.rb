d012cb19d408fe8f1e4edc364ccd739918c71738
jhovan1 <jahovanec@gmail.com>
2016-04-30 21:48:30 -0400

    Add defaults for products, avoid nulls

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ActiveRecord::Base
 	has_many :saleproducts
   	has_many :sales, through: :saleproducts
+  	after_initialize :init
+
+    def init
+	    self.name 				||= "" 
+	    self.description		||= "" 
+	    self.price				||= 0.0 
+	    self.commission_rate	||= 0.0 
+    end
 end

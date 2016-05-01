e9dabdaa9b2e5e1204c0ffdbdfc6c9f05aaa0e81
jhovan1 <jahovanec@gmail.com>
2016-04-30 22:00:38 -0400

    Add defaults to commissions, clients; avoid nulls

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,16 @@
 class Client < ActiveRecord::Base
 	has_many :sales
+    after_initialize :init
+
+    def init
+	    self.first_name		||= ""
+	    self.last_name		||= ""
+	    self.phone			||= ""
+	    self.address_line_1	||= ""
+	    self.address_line_2	||= ""
+	    self.city			||= ""
+	    self.state			||= ""
+	    self.country		||= ""
+	    self.zipcode		||= ""
+    end
 end

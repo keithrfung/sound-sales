16c62c1518e6f260bc799175516b5a041e0bb00c
Keith Fung <keith.robert.fung@gmail.com>
2016-05-01 18:03:15 -0400

    Fix Login Add session information

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20160501011121) do
   create_table "saleproducts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "sales_id"
+    t.integer  "products_id"
     t.integer  "sale_id"
     t.integer  "product_id"
   end
@@ -83,6 +85,9 @@ ActiveRecord::Schema.define(version: 20160501011121) do
     t.string   "last_name"
     t.string   "username"
     t.string   "password"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"

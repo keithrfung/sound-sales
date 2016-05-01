fcfd8445eb7b61922c0ee23fa6303ab0130c4e93
jhovan1 <jahovanec@gmail.com>
2016-04-30 21:23:46 -0400

    Save users with default values, prevent null values

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ActiveRecord::Base
 	has_many :subordinates, class_name: "User",
                           foreign_key: "manager_id"
     belongs_to :manager, class_name: "User"
+    after_initialize :init
+
+    def init
+		self.first_name	||= "" 
+	    self.last_name	||= "" 
+	    self.username	||= "" 
+	    self.password	||= "" 
+	    self.role		||= "" 
+	    self.manager_id	||= 0.0  
+	    self.region_id	||= 0.0  
+    end
 end

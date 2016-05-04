dc83a6380cc13cc1390fdb271e18ad05434f979f
Keith Fung <keith.robert.fung@gmail.com>
2016-05-04 16:57:09 -0400

    Hot Fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
 def dashboard
     @sales_count = Sale.all.count
-    @commissions_count = Commission.all.count
+    @commissions_count = Sale.all.where(user_id: current_user.id).count
 end
 
 end

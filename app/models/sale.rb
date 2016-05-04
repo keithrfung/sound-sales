5e04538be2f6e542131fe5b9f41c50289aa21b3f
Keith Fung <keith.robert.fung@gmail.com>
2016-05-03 23:08:11 -0400

    Hot fix for sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,12 +1,15 @@
 class Sale < ActiveRecord::Base
-	validates :product_id, presence: true
+	#validates :product_id, presence: true
 	validates :quantity, :numericality => { :greater_than => 0 }
 
 	belongs_to :user
 	belongs_to :client
-	#has_many :saleproducts
-  	has_one :product #s, through: :saleproducts
+
+  	has_one :product
   	has_one :commission
+	  
+	accepts_nested_attributes_for :product
+	
   	after_initialize :init
 	before_save :calculate
 
@@ -17,12 +20,13 @@ class Sale < ActiveRecord::Base
 	    self.sale_date		||= "" 
 	    self.user_id		||= 0.0  
 	    self.client_id		||= 0.0 
-	    self.product_id		||= 0.0 
+	    self.product_id		  
 	    self.commission_id	||= 0.0  
     end
 	
 	def calculate
-		self.subtotal = self.quantity * 4.00 #FIXME
+		
+		self.subtotal = self.quantity * Product.find(product_id).price
 		self.total = (1 + tax / 100) * self.subtotal
 	end
 end
